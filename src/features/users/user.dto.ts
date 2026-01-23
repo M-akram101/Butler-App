9b7a697757c4c461d5196248a73b765b003d18ef
Mohamed Akram <mohamed@d1al.com>
2026-01-23 23:57:36 +0200

    Changed number to -> phoneNumber and type too and li mited it to 11 chars

--- src/features/users/user.dto.ts
+++ src/features/users/user.dto.ts
@@ -1,25 +1,29 @@
-import * as z from "zod";
-import { Role, Industry, MaritalStatus, Gender } from '../../../prisma/generated/prisma/enums';
+import * as z from 'zod';
+import {
+  Role,
+  Industry,
+  MaritalStatus,
+  Gender,
+} from '../../../prisma/generated/prisma/enums';
 
 const UserSchema = z.object({
-
-  firstName: z.string().min(1, "First name is required"),
-  lastName: z.string().min(1, "Last name is required"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
-  address: z.string().max(100, "Address must be under 100 characters"),
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
+  password: z.string().min(8, 'Password must be at least 8 characters'),
+  address: z.string().max(100, 'Address must be under 100 characters'),
   // set age from date of birthday
-  email: z.string().email("Invalid email format"),
-  number: z.number().int().positive(),
+  email: z.string().email('Invalid email format'),
+  phoneNumber: z.string().max(11),
   role: z.nativeEnum(Role),
   industry: z.nativeEnum(Industry),
   maritalStatus: z.nativeEnum(MaritalStatus),
   gender: z.nativeEnum(Gender),
-  jobTitle: z.string().max(50, "Job title is too long"),
+  jobTitle: z.string().max(50, 'Job title is too long'),
   dateOfBirth: z.coerce.date(),
 });
 
-export const CreateUserSchema = UserSchema
-export const UpdateUserSchema = UserSchema.partial().omit({email:true})
+export const CreateUserSchema = UserSchema;
+export const UpdateUserSchema = UserSchema.partial().omit({ email: true });
 
-export type CreateUserInput = z.infer <typeof CreateUserSchema>;
+export type CreateUserInput = z.infer<typeof CreateUserSchema>;
 export type UpdateUserInput = z.infer<typeof UpdateUserSchema>;
