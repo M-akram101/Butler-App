02ef98507cd6a6fd0476c688775dfc9749ed67ea
Mohamed Akram <mohamed@d1al.com>
2026-01-31 00:02:06 +0200

    app and server updates

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
 import express from 'express';
-const morgan = require('morgan');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const xss = require('xss');
-import { errorHandler } from './middlewares/errorhandling';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import xss from 'xss';
 
+import { errorHandler } from './middleware/errorHandling';
+import { requestLogger } from './middleware/requestLogger';
 const app = express();
+//// Security Middlewares
 
-// For development only
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 // Security Headers
 app.use(helmet());
-
 // Rate Limiter
 const limiter = rateLimit({
   max: 100,
@@ -20,13 +19,17 @@ const limiter = rateLimit({
 });
 app.use(express.json({ limit: '10kb' }));
 app.use(xss());
-// Global error handler (should be after routes)
 
+// Development logger
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-
-
+// Request logger
+app.use(requestLogger);
 // Routes
 // app.use('/api/v1/users', userRouter);
+
+// Global error handler (should be after routes)
+
 app.use(errorHandler);
 
 export default app;
