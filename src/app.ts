dc42a2d3db543f31544e98ee54168ecd236b5c9b
Mohamed Akram <mohamed@d1al.com>
2026-01-30 17:15:40 +0200

    Added security middlewares (xss,rate limiter, security headers), and local loggerusing morgan

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,32 @@
 import express from 'express';
-import {errorHandler} from './middlewares/errorhandling';
-
-
+const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const xss = require('xss');
+import { errorHandler } from './middlewares/errorhandling';
 
 const app = express();
 
-app.use(express.json());
+// For development only
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+// Security Headers
+app.use(helmet());
 
-
-
+// Rate Limiter
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this Ip, Please try again in an hour !!',
+});
+app.use(express.json({ limit: '10kb' }));
+app.use(xss());
 // Global error handler (should be after routes)
+
+
+
+
+// Routes
+// app.use('/api/v1/users', userRouter);
 app.use(errorHandler);
 
 export default app;
